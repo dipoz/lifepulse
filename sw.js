e108ed405bbbc913eedfaf366d24952be56a9098
dipoz <59764778+dipoz@users.noreply.github.com>
2026-04-08 16:34:00 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,17 @@
-const CACHE_NAME = 'habits-v1';
-const ASSETS = ['/', '/stats', '/static/app.js', '/static/manifest.json'];
+const CACHE_NAME = 'lifepulse-v2';
+const ASSETS = ['./', './index.html', './app.js', './manifest.json'];
 
 self.addEventListener('install', e => {
     e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+        ))
+    );
 });
 
 self.addEventListener('fetch', e => {
